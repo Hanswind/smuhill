9fb721e6c58ad97eaa0d0dd9fce75c6db292ad11
CHOIKEONWOO <46857207+CHOIKEONWOO@users.noreply.github.com>
2019-07-26 00:54:59 +0900

    drag,snap
    그래그 기능, 가까워 지면 붙는 기능(snap) 추가, 드래그 하여 추가하는 기능 미구현

---
+++ Web Files/bootstrap/js/main.js
@@ -0,0 +1,11 @@
+
+$(".form1")
+.draggable({
+    containment:"parent",
+    scroll:true,
+    scrollSensitivity:100,
+    snap: ".outer", 
+    snapMode: "outer",
+    snapTolerance:10,
+    zIndex:10    
+});
