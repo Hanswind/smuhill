8baa1ff35cf61b7887f4847df146ab1110aed44e
CHOIKEONWOO <46857207+CHOIKEONWOO@users.noreply.github.com>
2019-07-28 11:37:23 +0900

    클릭하여 요소 추가
    버튼을 클릭하여 svg에 요소 추가

--- Web Files/bootstrap/js/main.js
+++ Web Files/bootstrap/js/main.js
@@ -1,11 +1,18 @@
-
-$(".form1")
-    .draggable({
-        containment:"parent",
-        scroll:true,
-        scrollSensitivity:100,
-        snap: ".outer",
-        snapMode: "outer",
-        snapTolerance:10,
-        zIndex:10
-    });
+var i=1;
+$(".templete1").click(function(){
+    var $element=$('<div class="form1 outer seq'+i+'" xmlns="http://www.w3.org/1999/xhtml"/>').text("폼1");
+    $(".svg-foreign").append($element);
+    $element.css({
+        "margin":"20px"
+    })
+    $element.draggable();
+    i++;
+});
+$(".templete2").click(function(){
+    var $element=$('<div class="form1 outer" xmlns="http://www.w3.org/1999/xhtml"/>').text("폼2");
+    $(".svg-foreign").append($element)
+    $element.css({
+        "margin":"20px"
+    })
+    $element.draggable();
+});
