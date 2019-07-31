9e890e155e60ee5aba95b2689d776ba4f29ec5be
ChayanRin <k852012@naver.com>
2019-07-31 16:21:31 +0900

    Update Web Files
    1. 요소 영역 수정
    2. drag and drop 기본 구현

--- Web Files/bootstrap/js/main.js
+++ Web Files/bootstrap/js/main.js
@@ -16,3 +16,18 @@ $(".templete2").click(function(){
     })
     $element.draggable();
 });
+
+/* drag and drop */
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
