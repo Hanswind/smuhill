e22d1fe7c322d64143052c5a956d239c5fe9d2c7
ChayanRin <k852012@naver.com>
2019-08-13 17:15:51 +0900

    레이아웃 접기, 반응형 레이아웃 추가

--- Web Files/bootstrap/js/main.js
+++ Web Files/bootstrap/js/main.js
@@ -19,6 +19,47 @@ $(".templete2").click(function(){
     $element.draggable();
     
 });
+
+
+
+/* 레이아웃 접기*/
+function fold_layout(){
+    if(document.getElementById("fold_button").value == ">>"){
+        document.getElementById("fold_button").value='<<';
+        $(function(){
+            $("#right_section").animate({width:20});
+            $("#fold_button").animate({right:0});
+
+            if (matchMedia("screen and (min-width:768px)").matches) {
+                var nextWidth = $(".section2").width()+350;
+                $(".section2").animate({
+                    width:nextWidth
+                }, { complete:function(){ $(".section2").width("calc(100% - 350px)"); } });
+            }
+            else{}
+        });
+    }
+
+    else if(document.getElementById("fold_button").value == "<<"){
+        document.getElementById("fold_button").value=">>";
+        $(function(){
+            $("#right_section").animate({width:400});
+            $("#fold_button").animate({right:350});
+            if (matchMedia("screen and (min-width:768px)").matches) {
+                var nextWidth = $(".section2").width()-350;
+                $(".section2").animate({
+                    width:nextWidth
+                }, { complete:function(){ $(".section2").width("calc(100% - 700px)"); } });
+            }
+        });
+    }
+    else{}
+}
+
+
+
+
+
 /* drag and drop */
 function allowDrop(ev) {
     ev.preventDefault();
