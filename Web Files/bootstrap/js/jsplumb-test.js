abe0f1b1cc1cfc687bcdd3735f0814ac7866a820
CHOIKEONWOO <46857207+CHOIKEONWOO@users.noreply.github.com>
2019-08-15 20:55:23 +0900

    텍스트요소 connect 개선

---
+++ Web Files/bootstrap/js/jsplumb-test.js
@@ -0,0 +1,74 @@
+var numberOfTemplete1=0;
+var numberOfTemplete2=0;
+jsPlumb.ready(function () {
+
+
+    var instance = jsPlumb.getInstance({
+        DragOptions: { cursor: 'pointer' },
+        PaintStyle: { stroke: '#666' },
+        EndpointHoverStyle: { fill: "orange" },
+        HoverPaintStyle: { stroke: "orange" },
+        EndpointStyle: { width: 20, height: 16, stroke: '#666' },
+        Endpoint: "Rectangle",
+        Anchors: ["TopCenter", "TopCenter"],
+        Container: "svg-foreign",
+        MaxConnections: 1,
+        ConnectionsDetachable: false,
+    });
+
+    instance.batch(function () {
+        var color1 = "#ff0000";
+        var exampleEndpoint1 = {
+            endpoint: ["Dot", { radius: 11 }],
+            paintStyle: { fill: color1 },
+            isSource: false,
+            scope: "green",
+            connectorStyle: { stroke: color1, strokeWidth: 6 },
+            connector: ["Bezier", { curviness: 63 } ],
+            maxConnections: 3,
+            isTarget: true,
+        };
+        var color2 = "#316b31";
+        var exampleEndpoint2 = {
+            endpoint: ["Dot", { radius: 11 }],
+            paintStyle: { fill: color2 },
+            isSource: true,
+            scope: "green",
+            connectorStyle: { stroke: color2, strokeWidth: 6 },
+            connector: ["Bezier", { curviness: 63 } ],
+            maxConnections: 3,
+            isTarget: false,
+        };
+
+
+        var startpoint = {
+            isSource: true,
+            isTarget: false,
+            maxConnections: 1,
+            anchor: "BottomCenter"
+          };
+        jsPlumb.addEndpoint($(".entry"),{anchor:"Bottom"},exampleEndpoint2);
+        jsPlumb.draggable($(".entry"));
+        $(".templete1").click(function(){
+            addTemplete1();
+        });
+        $(".templete2").click(function(){
+            addTemplete2();
+        });
+
+        function addTemplete1(id){
+            if(typeof id==="undefined"){
+                numberOfTemplete1++;
+                id="templete1_"+numberOfTemplete1;
+            }
+            var $element=$('<div class="templete1-body window" id="'+id+'"><div class="templete1-uparrow">&#8593</div><div class="templete1-text" >text'+numberOfTemplete1+'</div><div class="templete1-downarrow" id="source">&#8595</div></div>');
+            $(".svg-foreign").append($element);
+            jsPlumb.addEndpoint($("#"+id),{anchor:"TopCenter"},exampleEndpoint1);
+            jsPlumb.addEndpoint($("#"+id),{anchor:"BottomCenter"},exampleEndpoint2);
+            jsPlumb.draggable($("#"+id));
+        };
+    });
+    
+    jsPlumb.fire("jsPlumbDemoLoaded", instance);
+    
+});
--- Web Files/bootstrap/js/main.js
+++ Web Files/bootstrap/js/main.js
@@ -1,21 +1,7 @@
-var i=1;
-$(".templete1").click(function(){
-    var $element=$('<div class="templete1-body"><div class="templete1-uparrow">&#8593</div><div class="templete1-text" >text</div><div class="templete1-downarrow">&#8595</div></div>')
-    $(".svg-foreign").append($element);
-    $element.css({
-        "margin":"20px"
-    });
-    $element.draggable();
-    i++;
-});
-$(".entry").draggable();
 
 $(".templete2").click(function(){
     var $element=$('<div class="templete2-body"><div class="uparrow">&#8593</div><div class="templete2-text">text</div><div class="templete2-buttonbox"><button class="templete2-button btn1">+</button><button class="templete2-button btn2">+</button><button class="templete2-button btn3">+</button></div><div class="downarrow">&#8595</div></div>');
     $(".svg-foreign").append($element);
-    $element.css({
-        "margin":"20px"
-    });
     $element.draggable();
     
 });
@@ -76,31 +62,4 @@ function drop(ev) {
     ev.target.appendChild(document.getElementById(data));
 }
 
-const jsPlumbOptions = {
-    Connector: ['Straight'],
-    Container: "connections",
-    PaintStyle: {
-      strokeWidth: 2,
-      stroke: '#eee',
-      outlineWidth: 2,
-    },
-    Endpoint: [ "Dot", { radius: 1 } ],
-    ConnectionOverlays: [
-      ['Arrow', {
-        location: 1,
-        visible: true,
-        width: 15,
-        length: 15,
-        foldback: 1,
-      }],
-    ],
-  };
-  
-  const jsPlumbInstance = jsPlumb.getInstance(jsPlumbOptions);
-  
-  jsPlumbInstance.makeSource('source', {
-    anchor: "Center"
-  });
-  jsPlumbInstance.makeTarget("target", {
-    anchor: "Center"
-  });
+
