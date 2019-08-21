581b6eac041fc56916d32d26e11b4a86ebc1df4b
ChayanRin <k852012@naver.com>
2019-08-21 14:54:59 +0900

    요소 선택 버튼 추가 및 전체적 개선

--- Web Files/bootstrap/js/main.js
+++ Web Files/bootstrap/js/main.js
@@ -34,20 +34,72 @@ function fold_layout(){
     }
     else{}
 }
+/* 요소 선택 */
+
+function select(num) {
+    if (num == 1) {
+        if ($("#s_all").attr("value") != "s") {
+            $("#normal_elements, #template, #adv_elements, #adv_template, #download_elements").css("display", "block");
+            $("#s_all").attr("value", "s");
+            $("#s_bas").attr("value", "c");
+            $("#s_down").attr("value", "c");
+        }
+    } else if (num == 2) {
+        if ($("#s_bas").attr("value") != "s") {
+            $("#normal_elements, #template, #adv_elements, #adv_template").css("display", "block");
+            $("#download_elements").css("display", "none");
+            $("#s_all").attr("value", "c");
+            $("#s_bas").attr("value", "s");
+            $("#s_down").attr("value", "c");
+        }
+    } else if (num == 3) {
+        if ($("#s_down").attr("value") != "s") {
+            $("#normal_elements, #template, #adv_elements, #adv_template").css("display", "none");
+            $("#download_elements").css("display", "block");
+            $("#s_all").attr("value", "c");
+            $("#s_bas").attr("value", "c");
+            $("#s_down").attr("value", "s");
+        }
+    }
+
+    _select_("#s_all");
+    _select_("#s_bas");
+    _select_("#s_down");
+}
+
+function _select_(id) {
+    if ($(id).attr("value") == "s") {
+        $(id).css("top", 0);
+        $(id).css("font-weight", "bold");
+        $(id).css("background-color", "rgba(255,192,0)");
+    }
+    else {
+        $(id).css("top", 5);
+        $(id).css("font-weight", "normal");
+        $(id).css("background-color", "rgba(255,192,0,0.2)");
+    }
+}
+
+
+
 
 /* 기본요소 접기 */
 function fold_normal(){
     if($("#normal_elements").attr("value") == "open"){
         $("#template").animate({height:0});
-        $("#normal_elements").attr("value", "close")
-        $("#fold1").css("display", "none")
-        $("#fold2").css("display", "inline")
+        $("#normal_elements").attr("value", "close");
+        $("#fold1").css("display", "none");
+        $("#fold2").css("display", "inline");
+        $("#normal_elements").css("background-color", "#dddddd");
+        $("#normal_elements").css("color", "#999");
     }
     else{
         $("#template").animate({height:430});
         $("#normal_elements").attr("value", "open")
         $("#fold1").css("display", "inline")
         $("#fold2").css("display", "none")
+        $("#normal_elements").css("background-color", "#ffffff");
+        $("#normal_elements").css("color", "#000");
     }
 }
 
@@ -58,12 +110,16 @@ function fold_adv(){
         $("#adv_elements").attr("value", "close")
         $("#fold3").css("display", "none")
         $("#fold4").css("display", "inline")
+        $("#adv_elements").css("background-color", "#dddddd");
+        $("#adv_elements").css("color", "#999");
     }
     else{
         $("#adv_template").animate({height:330});
         $("#adv_elements").attr("value", "open")
         $("#fold3").css("display", "inline")
         $("#fold4").css("display", "none")
+        $("#adv_elements").css("background-color", "#ffffff");
+        $("#adv_elements").css("color", "#000");
     }
 }
 
