53348152530ea6ed069b0e1cbe33431d1f3b9d03
CHOIKEONWOO <46857207+CHOIKEONWOO@users.noreply.github.com>
2019-09-12 19:43:38 +0900

    form

--- Web Files/bootstrap/js/jsplumb-test.js
+++ Web Files/bootstrap/js/jsplumb-test.js
@@ -106,12 +106,14 @@ jsPlumb.ready(function () {
                 numberOfElements++;
                 numberOfTemplete1++;
                 id = "templete1_" + numberOfTemplete1;
+
             }
-            var $element=$('<div class=" templete1-body window" id="'+id+'" draggable="true"><form action="./text" method="POST"><input type="submit" value="제출" /></form><div class="text_name"><div class="name_content"  contenteditable="true" onclick="$(this).focus();" >텍스트'+numberOfTemplete1+'</div></div><div class="text_content" contenteditable="true" onclick="$(this).focus();" data-text="챗봇에 보여질 텍스트 내용을 작성하세요!"></div></div>');
+            var $element=$('<div class=" templete1-body window" id="'+id+'" draggable="true"><form action="/topic/create_process" method="post"><p><input type="text" name="title" placeholder="할일 제목"></p><p><textarea name="description" placeholder="할일 내용"></textarea></p><p><input type="submit"></p></form><div class="text_name"><div class="name_content"  contenteditable="true" onclick="$(this).focus();" >텍스트'+numberOfTemplete1+'</div></div><div class="text_content" contenteditable="true" onclick="$(this).focus();" data-text="챗봇에 보여질 텍스트 내용을 작성하세요!"></div></div>');
             $(".svg-foreign").append($element);
             jsPlumb.draggable($("#" + id));
             jsPlumb.addEndpoint($("#" + id), { anchor: "TopCenter" }, exampleEndpoint1);
             jsPlumb.addEndpoint($("#" + id), { anchor: "BottomCenter" }, exampleEndpoint2);
+            
         };
         function addTemplete2(id) {
             if (typeof id === "undefined") {
